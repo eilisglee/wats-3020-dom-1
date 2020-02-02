61c40a204d9c294f5e69cb775596c37822b9c2e2
eilisglee <elee2@seattleu.edu>
2020-02-01 18:27:59 -0800

    finished part 3

---
+++ 3-list/js/index.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let itemInput = document.querySelector("input[name='item-input']");
+    let itemValue = itemInput.value;
+
+    //test to be sure that user entered a value
+    if (itemValue.length !== 0) {
+      //create new item
+      let newItemEl = document.createElement("li");
+      let newCheckBox = document.createElement("input");
+      newCheckBox.setAttribute("type", "checkbox");
+      newItemEl.appendChild(newCheckBox);
+      let newLabel = document.createElement("label");
+      newLabel.textContent = itemValue;
+      newItemEl.appendChild(newLabel);
+
+      //get a reference to list and append list item
+      let list = document.querySelector('.todo-list');
+      list.appendChild(newItemEl);
+      //clear the user input
+      itemInput.value = '';
+
+      //add a click event listener that serves a toggle
+      newCheckBox.addEventListener("click", function (event) {
+        let doneItem = document.createElement("li");
+        doneItem.textContent = this.nextSibling.textContent;
+        document.getElementById("done-list").appendChild(doneItem);
+        this.parentElement.remove();
+
+      });
+
+    }
+  }) // form submit
+}) // document ready
